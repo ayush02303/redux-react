fb3ae810f983b924c24e9870678adfad622ba25f
ayush02303 <badolaayush295@gmail.com>
2025-11-11 15:01:09 +0530

    changes 2

--- src/store.jsx
+++ src/store.jsx
@@ -1,21 +1,26 @@
-const ADD_TASK = "task/add"
+const ADD_TASK = "task/add";
+const DELETE_TASK = "task/delete";
 
-const intialState = {
-    task : []
-}
+const initialState = {
+  task: [],
+};
 
-const taskReducer = ( state = intialState , action )=>{
-    switch (action.type) {
+const taskReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_TASK:
+      return {
+        ...state,
+        task: [...state.task, action.payload],
+      };
 
-        case ADD_TASK:
-            return {
-                
-            }
-            
-            break;
-    
-        default:
-            break;
-    }
+    case DELETE_TASK:
+      const updatedTask = state.task.filter((currTask, index) => index !== action.payload);
+      return {
+        ...state,
+        task: updatedTask,
+      };
 
-} 
+    default:
+      return state;
+  }
+};
